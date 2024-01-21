0d77b1e6b7833c925b1edb16d726aab375bf8d42
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2024-01-21 12:08:17 +0100

    done task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-
 const filePath = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(filePath);
 stream.on('data', (data) => {
