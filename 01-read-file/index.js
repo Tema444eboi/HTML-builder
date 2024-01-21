090e86e1ad901de88c4f41ae90c488168402e6e8
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2024-01-21 12:06:43 +0100

    Update index.js

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(filePath);
+stream.on('data', (data) => {
+  console.log(data.toString());
+});
