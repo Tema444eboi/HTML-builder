067bf3869803909ffbacfa3e7f395e15ddee8041
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2024-01-21 12:13:27 +0100

    done task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+const { stdin, stdout, exit } = process;
+
+stdout.write(
+  'Please, enter your text to write it to file text.txt.\n' +
+    'To exit press Ctrl + C or enter: exit\n',
+);
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') exit();
+  writeStream.write(data.toString());
+});
+
+process.on('exit', () => stdout.write('You are leaving this program ?'));
+process.on('SIGINT', exit);
